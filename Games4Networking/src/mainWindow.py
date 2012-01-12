ff3ece45e8fab294a78777b7abe9bd42232c0c46
pfl <pfl@eal.dk>
2012-01-12 11:24:49 +0100

    added a mainwindow

---
+++ Games4Networking/src/mainWindow.py
@@ -0,0 +1,16 @@
+'''
+Created on Jan 12, 2012
+
+@author: pfl
+'''
+
+'''
+You all need to add a button to this main window. Pushing your 
+'''
+
+if __name__ == '__main__':
+    import tkinter
+
+    widget = tkinter.Label(None, text="Hello guys !")
+    widget.pack()
+    widget.mainloop()
