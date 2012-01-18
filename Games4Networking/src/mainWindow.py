79e38ae53ee5f2474b046d126f4a47b3015e4812
Victor <romanescu3@gmail.com>
2012-01-18 11:43:38 +0100

    removed the popup from main

--- Games4Networking/src/mainWindow.py
+++ Games4Networking/src/mainWindow.py
@@ -19,31 +19,7 @@ If you want the latest from some elses repository (maybe mine) you must do this:
             "
 '''
 import tkinter as tk
- 
-def popout():
-    def currencyconv():
-        print("what do you want to exchange")
-        what = input()
-        print("and how much")
-        x = int(input())
-        if what == "ron" or "lei":
-            euro = x*0.23
-            dkk = x*1.71
-            return(x,what, "inseamna",euro,"euroi si",dkk,"coroane")
-        if what == "euro":
-            ron = x*4.3
-            dkk = x*7.4
-            return(x,what, "inseamna",ron,"ron si",dkk,"coroane")
-        if what == "dkk":
-            ron = x*6
-            euro = x*0.13
-            return(x,what, "inseamna",ron,"ron si",euro,"euro") 
-    other = tk.Toplevel()
-    other.title("Victors Window")
-    otherlabel = tk.Label(other, text='this is it', relief = tk.RIDGE)
-    otherlabel.pack(side=tk.TOP, fill = tk.BOTH, expand = tk.YES)
-    otherbutton = tk.Button(other, text='CLICK ME').pack(side=tk.TOP)
-    otherbutton = tk.Button(other, text='NO,NO, CLICK ME').pack(side=tk.BOTTOM)
+import mypopup.popup as victorpopup
     
 
 if __name__ == '__main__':
@@ -56,7 +32,7 @@ if __name__ == '__main__':
     # Everyone should add their own button like this 
     
     buttonGameFlindt = tk.Button( win, text = "a puzzle", command = win.quit).pack(side=tk.TOP)
-    buttonVictor = tk.Button( win, text="TEST", command=popout).pack(side=tk.BOTTOM)
+    buttonVictor = tk.Button( win, text="TEST", command=victorpopup.popout).pack(side=tk.BOTTOM)
     buttonQuit = tk.Button( win, text = "Quit", command = win.quit).pack(side=tk.RIGHT)    
     
 
